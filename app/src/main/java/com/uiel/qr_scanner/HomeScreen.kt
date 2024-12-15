5c122d51de75a5ed62ce27b716c011f7293dabf3
uiel <parkuiery@gmail.com>
2024-12-15 23:56:51 +0900

    feat :: navcontroller 추가

--- app/src/main/java/com/uiel/qr_scanner/HomeScreen.kt
+++ app/src/main/java/com/uiel/qr_scanner/HomeScreen.kt
@@ -5,16 +5,17 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.navigation.NavController
 
 @Composable
 fun HomeScreen(
-    modifier: Modifier = Modifier
+    navController: NavController,
 ) {
     Column {
         Button(onClick = { /*TODO*/ }) {
             Text(text = "사진")
         }
-        Button(onClick = { /*TODO*/ }) {
+        Button(onClick = { navController.navigate("scan") }) {
             Text(text = "카메라")
         }
     }
